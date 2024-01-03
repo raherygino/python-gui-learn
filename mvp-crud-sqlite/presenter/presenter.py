1afba937be7d89ca516740bedba9b0388eb3103b
raherygino <georgino197@gmail.com>
2024-01-03 07:25:28 +0100

    Create presenter

---
+++ mvp-crud-sqlite/presenter/presenter.py
@@ -0,0 +1,22 @@
+class Presenter:
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+        self.populate_table()
+        self.view.show()
+
+    def populate_table(self):
+        items = self.model.fetch_all_items()
+        self.view.populate_table(items)
+
+    def add_item(self, name):
+        self.model.add_item(name)
+        self.populate_table()
+
+    def update_item(self, item_id, new_name):
+        self.model.update_item(item_id, new_name)
+        self.populate_table()
+
+    def delete_item(self, item_id):
+        self.model.delete_item(item_id)
+        self.populate_table()
