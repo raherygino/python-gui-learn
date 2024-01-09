fb630aaebf825edba2cff5806be4577904da5fa3
raherygino <georgino197@gmail.com>
2024-01-09 07:36:17 +0100

    Update view

--- mvp-crud-sqlite/view/view.py
+++ mvp-crud-sqlite/view/view.py
@@ -11,15 +11,21 @@ class View(QMainWindow):
 
         self.name_label = QLabel("Name:")
         self.name_edit = QLineEdit()
+
+        self.firstname_label = QLabel("Firstname:")
+        self.firstname_edit = QLineEdit()
+
         self.add_button = QPushButton("Add")
         self.add_button.clicked.connect(self.add_clicked)
 
         self.table = QTableWidget()
-        self.table.setColumnCount(2)
-        self.table.setHorizontalHeaderLabels(["ID", "Name"])
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(["ID", "Name", "Firstname"])
 
         self.layout.addWidget(self.name_label)
         self.layout.addWidget(self.name_edit)
+        self.layout.addWidget(self.firstname_label)
+        self.layout.addWidget(self.firstname_edit)
         self.layout.addWidget(self.add_button)
         self.layout.addWidget(self.table)
 
@@ -27,8 +33,10 @@ class View(QMainWindow):
 
     def add_clicked(self):
         name = self.name_edit.text()
-        self.presenter.add_item(name)
+        firstname = self.firstname_edit.text()
+        self.presenter.add_item(name, firstname)
         self.name_edit.clear()
+        self.firstname_edit.clear()
 
     def populate_table(self, items):
         self.table.setRowCount(0)
